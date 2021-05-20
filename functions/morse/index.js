3a41de36cf56475611af204a25b0c642bf76ccee
Filip <31472895+Filiq@users.noreply.github.com>
2021-05-20 22:26:54 +0200

    Add files via upload

--- functions/morse/index.js
+++ functions/morse/index.js
@@ -1,13 +1,16 @@
 import morseEncode from "./morseEncode";
 import morseDecode from "./morseDecode";
 
-const morse = (str, type) => {
+const morse = (str, subcategory) => {
   if (str === "") return "";
+
+  const { type, short, long, space } = subcategory;
+
   switch (type) {
     case "encode":
-      return morseEncode(str);
+      return morseEncode(str, short, long, space);
     case "decode":
-      return morseDecode(str);
+      return morseDecode(str, short, long, space);
     default:
       return str;
   }
